7d33e0e3d97a27631a86403e307ea74c3b38dc55
mb16biswas <64213233+mb16biswas@users.noreply.github.com>
2021-03-30 11:07:38 +0530

    Add files via upload

---
+++ build/precache-manifest.5463fc446865eb131640d7d80de74e82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8f3cb5ae297f9fe344cb8ab253bdc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b6882191bc94dd431a7",
+    "url": "/static/css/2.89d568a6.chunk.css"
+  },
+  {
+    "revision": "3b6882191bc94dd431a7",
+    "url": "/static/js/2.076f1a3a.chunk.js"
+  },
+  {
+    "revision": "720f54caf9a00ba94276",
+    "url": "/static/js/main.7c6a5fe5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
