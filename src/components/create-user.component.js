1ef0c122ec4651a19ff627e835614e9b9a565e09
mb16biswas <64213233+mb16biswas@users.noreply.github.com>
2021-03-30 11:09:29 +0530

    Update create-user.component.js

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -28,7 +28,7 @@ export default class CreateUser extends Component {
 
     console.log(user);
 
-    axios.post('https://gym-tracker-back.herokuapp.com/users/add', user)
+    axios.post(`${process.env.REACT_APP_BACKEND_URL}users/add`, user)
       .then(res => console.log(res.data));
 
     this.setState({
